8b8a4bb0de7280999544c1910216378c7d4bf49d
iPsycho <125604888+iPsycho1@users.noreply.github.com>
2025-05-07 13:32:51 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -9,6 +9,7 @@ SOURCE_URLS = [
     "https://raw.githubusercontent.com/NiREvil/vless/main/sub/SSTime",
     "https://panel.maviks.eu/sub/NjE3Mzg1MTE4MywxNzQ1NjYzODY5ac1o7kfeLv",
     "https://raw.githubusercontent.com/iPsycho1/Subscription/refs/heads/main/iPsycho_Location",
+    "https://raw.githubusercontent.com/iPsycho1/wg-config-fetcher/refs/heads/main/configs/wireguard_configs.txt",
     # Add more URLs here if you want to include additional sources.
 ]
 
